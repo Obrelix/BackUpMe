54182123b8bfb2e0c46bef3fbeed74b2848bea4d
Obrelix <Obrelix@OBRELIXPC>
2017-02-24 13:05:36 +0200

    Added Folders to Repo

--- BackUpMe/Properties/Resources.Designer.cs
+++ BackUpMe/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace BackUpMe.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon backup {
+            get {
+                object obj = ResourceManager.GetObject("backup", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
